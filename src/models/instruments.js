e5d6a8813a090a12546b6ef4cf668add068110de
Hamish-H <hamish.h@outlook.com>
2018-06-26 22:30:30 +0100

    updated instruments file

---
+++ src/models/instruments.js
@@ -0,0 +1,14 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const Instruments = function(instrument_families) {
+  this.instrument_families = instrument_families;
+};
+
+
+
+
+
+
+
+
+module.exports = Instruments;
