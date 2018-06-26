dd88449249963185924bc5794a10512414d611ab
Hamish-H <hamish.h@outlook.com>
2018-06-26 22:32:14 +0100

    updated syntax

--- src/models/instruments.js
+++ src/models/instruments.js
@@ -1,7 +1,7 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const Instruments = function(instrument_families) {
-  this.instrument_families = instrument_families;
+const Instruments = function(instrumentFamilies) {
+  this.instrumentFamilies = instrumentFamilies;
 };
 
 
