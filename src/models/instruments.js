8021d603be29bf05be01c8c8236f0e27b1539645
Hamish-H <hamish.h@outlook.com>
2018-06-26 22:26:25 +0100

    indentation

--- src/models/instruments.js
+++ src/models/instruments.js
@@ -8,9 +8,9 @@ SelectView.prototype.bindEvents = function() {
   this.menuItems.forEach((menuItem) => {
     menuItem.addEventListener('click', (evt) => {
       const selectedInstrumentName = evt.target.id;
-            PubSub.publish('SelectView:selected', selectedInstrumentName);
-          });
-        });
-      };
+      PubSub.publish('SelectView:selected', selectedInstrumentName);
+      });
+  });
+};
 
-      module.exports = SelectView;
+module.exports = SelectView;
