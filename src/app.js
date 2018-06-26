99af49333dd56edf07787f62fa500b7c17bfa3ff
Hamish-H <hamish.h@outlook.com>
2018-06-27 00:04:00 +0100

    requires

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-// const         = require('./models/instrument_families.js');
-// const         = require('./models/instrument.js');
-
-
+const         = require('./models/instrument_families.js');
+const         = require('./models/instrument.js');
+const         = require('./views/result_view.js');
+const         = require('./views/select_view.js');
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
 });
