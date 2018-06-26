636ee7c3f3531699a51e165c9ee013c45ca26b4f
Hamish-H <hamish.h@outlook.com>
2018-06-26 22:27:20 +0100

    uopdated select_view instruments files

---
+++ src/views/select_view.js
@@ -0,0 +1,16 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const SelectView = function(menuItems) {
+  this.menuItems = menuItems;
+};
+
+SelectView.prototype.bindEvents = function() {
+  this.menuItems.forEach((menuItem) => {
+    menuItem.addEventListener('click', (evt) => {
+      const selectedInstrumentName = evt.target.id;
+      PubSub.publish('SelectView:selected', selectedInstrumentName);
+      });
+  });
+};
+
+module.exports = SelectView;
