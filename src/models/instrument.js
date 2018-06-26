7f5e536bb266d9d9b315b5aecf5d30d47f35729f
Hamish-H <hamish.h@outlook.com>
2018-06-26 23:13:12 +0100

    updated syntax

--- src/models/instrument.js
+++ src/models/instrument.js
@@ -1,22 +1,22 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const Instruments = function(instrumentFamilies) {
+const Instrument = function(instrumentFamilies) {
   this.instrumentFamilies = instrumentFamilies;
 };
 
-Instruments.prototype.bindEvents = function () {
+Instrument.prototype.bindEvents = function () {
   PubSub.subscribe('SelectView:selected', (event) => {
     const chosenInstrumentName = event.detail;
     const selectedInstrumentObject = this.findByName(chosenInstrumentName);
-    PubSub.publish('Instruments:instrument-ready', selectedInstrumentObject);
+    PubSub.publish('Instrument:instrument-ready', selectedInstrumentObject);
   });
 };
 
-Instruments.prototype.findByName = function(searchName) {
+Instrument.prototype.findByName = function(searchName) {
   const foundInstrument = this.instrument.find((currentInstrument) => {
     return currentInstrument.name === searchName;
   });
   return foundInstrument;
 };
 
-module.exports = Instruments;
+module.exports = Instrument;
