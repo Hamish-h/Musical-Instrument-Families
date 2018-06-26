bf62f06301fe96ec5b315abf774c0c40c3008ddf
Hamish-H <hamish.h@outlook.com>
2018-06-26 22:49:17 +0100

    updated instruments

--- src/models/instruments.js
+++ src/models/instruments.js
@@ -5,12 +5,18 @@ const Instruments = function(instrumentFamilies) {
 };
 
 Instruments.prototype.bindEvents = function () {
-
+  PubSub.subscribe('SelectView:selected', (event) => {
+    const chosenInstrumentName = event.detail;
+    const selectedInstrumentObject = this.findByName(chosenInstrumentName);
+    PubSub.publish('Instruments:instrument-ready', selectedInstrumentObject);
+  });
 };
 
-
-
-
-
+Instruments.prototype.findByName = function(searchName) {
+  const foundInstrument = this.instrument.find((currentInstrument) => {
+    return currentInstrument.name === searchName;
+  });
+  return foundInstrument;
+};
 
 module.exports = Instruments;
